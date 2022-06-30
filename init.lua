8f01ad4059615b6bcf050ac99554b6031bf4f3f2
Lam Duong <lduong@jjrsolutions.com>
2022-06-30 15:33:11 -0400

    Lua-line added with Tokyo night theme

--- init.lua
+++ init.lua
@@ -196,6 +196,36 @@ cmp.setup.cmdline(':', {
   })
 })
 
+----------------------------- Lua-line --------------------------------
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'tokyonight',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = false,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
 ----------------------------- OTHERS --------------------------------
 -- Neogit: similar to Magit
 local neogit = require('neogit')
