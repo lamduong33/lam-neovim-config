b00dad47f95e894eba335e96722630ad678cb2a3
Lam T Duong <lamthachduong@gmail.com>
2023-02-02 14:40:38 -0500

    fix: Removed tokyonight and changed dashboard

--- init.lua
+++ init.lua
@@ -97,6 +97,9 @@ require'lspconfig'.texlab.setup{
 require'lspconfig'.clangd.setup{
   capabilities = capabilities
 }
+require'lspconfig'.jdtls.setup{
+  capabilities = capabilities
+}
 require'lspconfig'.eslint.setup{}
 --require'lspconfig'.tsserver.setup{
 --    cmd = { "typescript-language-server", "--stdio"},
@@ -275,33 +278,36 @@ require('lualine').setup {
 
 -- Dashboard 
 local home = os.getenv('HOME')
-local db = require('dashboard')
-db.custom_center = {
-  {icon = ' ',
-  desc = 'Reload Last Session                     ',
-  shortcut = 'SPC s l',
-  action ='SessionLoad'},
-  {icon = ' ',
-  desc = 'Recently opened files                   ',
-  action =  'Telescope oldfiles',
-  shortcut = 'SPC f r'},
-  {icon = ' ',
-  desc = 'Find  File                              ',
-  action = 'Telescope find_files find_command=rg,--hidden,--files',
-  shortcut = 'SPC f f'},
-  {icon = ' ',
-  desc = 'File Browser                            ',
-  action =  'Telescope file_browser',
-  shortcut = 'SPC .  '},
-  {icon = ' ',
-  desc = 'Find word                               ',
-  action = 'Telescope live_grep',
-  shortcut = 'SPC f w'},
-  {icon = ' ',
-  desc = 'Open NVim dotfiles                      ',
-  action = 'Telescope dotfiles path=' .. home ..'/.config/nvim',
-  shortcut = 'SPC f p'},
-}
+-- local db = require('dashboard')
+--db.custom_center = {
+--  {icon = ' ',
+--  desc = 'Reload Last Session                     ',
+--  shortcut = 'SPC s l',
+--  action ='SessionLoad'},
+--  {icon = ' ',
+--  desc = 'Recently opened files                   ',
+--  action =  'Telescope oldfiles',
+--  shortcut = 'SPC f r'},
+--  {icon = ' ',
+--  desc = 'Find  File                              ',
+--  action = 'Telescope find_files find_command=rg,--hidden,--files',
+--  shortcut = 'SPC f f'},
+--  {icon = ' ',
+--  desc = 'File Browser                            ',
+--  action =  'Telescope file_browser',
+--  shortcut = 'SPC .  '},
+--  {icon = ' ',
+--  desc = 'Find word                               ',
+--  action = 'Telescope live_grep',
+--  shortcut = 'SPC f w'},
+--  {icon = ' ',
+--  desc = 'Open NVim dotfiles                      ',
+--  action = 'Telescope dotfiles path=' .. home ..'/.config/nvim',
+--  shortcut = 'SPC f p'},
+--}
+-- db.custom_header=vim_header
+-- db.SessionLoad=true
+
 map('n', '<leader>sl', '<cmd>SessionLoad<CR>', opts)
 map('n', '<leader>fr', '<cmd>Telescope oldfiles<CR>', opts)
 map('n', '<leader>ff', '<cmd>Telescope find_files find_command=rg,--hidden,--files<CR>', opts)
@@ -330,8 +336,6 @@ local vim_header = {
 "                  .....                 ", 
 "                    .                   ",
 }
-db.custom_header=vim_header
-db.SessionLoad=true
 
 -- Telescope file browser
 require("telescope").load_extension "file_browser"
@@ -498,40 +502,40 @@ vim.g.nord_disable_background = true
 vim.g.nord_italic = true
 
 -- Tokyonight theme
-require("tokyonight").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  transparent = true, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "transparent", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
-  },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-})
+-- require("tokyonight").setup({
+--   -- your configuration comes here
+--   -- or leave it empty to use the default settings
+--   style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+--   transparent = true, -- Enable this to disable setting the background color
+--   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+--   styles = {
+--     -- Style to be applied to different syntax groups
+--     -- Value is any valid attr-list value for `:help nvim_set_hl`
+--     comments = { italic = true },
+--     keywords = { italic = true },
+--     functions = {},
+--     variables = {},
+--     -- Background styles. Can be "dark", "transparent" or "normal"
+--     sidebars = "transparent", -- style for sidebars, see below
+--     floats = "dark", -- style for floating windows
+--   },
+--   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+--   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+--   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+--   dim_inactive = false, -- dims inactive windows
+--   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+-- 
+--   --- You can override specific color groups to use other groups or a hex color
+--   --- function will be called with a ColorScheme table
+--   ---@param colors ColorScheme
+--   on_colors = function(colors) end,
+-- 
+--   --- You can override specific highlights to use other groups or a hex color
+--   --- function will be called with a Highlights and ColorScheme table
+--   ---@param highlights Highlights
+--   ---@param colors ColorScheme
+--   on_highlights = function(highlights, colors) end,
+-- })
 
 -- Gitsigns - showing git changes on the number line
 require('gitsigns').setup {
@@ -624,3 +628,6 @@ require('nvim-ts-autotag').setup()
 vim.g.leetcode_browser = "firefox";
 
 vim.cmd([[colorscheme nord]])
+
+-- nvim-colorizer setup
+require'colorizer'.setup()
